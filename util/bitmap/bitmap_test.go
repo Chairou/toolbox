80970ea082f9db0e90ff6ad839902fdbde27852e
chairou <chairou@tencent.com>
2023-03-07 19:54:27 +0800

    Improve the bitmap interface implementation

--- util/bitmap/bitmap_test.go
+++ util/bitmap/bitmap_test.go
@@ -3,9 +3,17 @@ package bitmap
 import "testing"
 
 func TestBitmap(t *testing.T) {
-	bitmap := GetBitMap("test1", 15)
-	bitmap.Set(0)
-	bitmap.MSet(2, 3, 4)
+	bitmap := NewBitMap("test1", 16)
+	err := bitmap.Set(0)
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = bitmap.MSet(2, 3, 4)
+	if err != nil {
+		t.Error(err)
+	}
+
 	matchMap := bitmap.MExist(1, 2, 3)
 	if matchMap[1] != false || matchMap[2] != true || matchMap[3] != true {
 		t.Error("UNExpected error")
@@ -15,13 +23,17 @@ func TestBitmap(t *testing.T) {
 		t.Error("UNExpected error")
 	}
 	bitmap.Clean()
-	bitmap.Set(15)
+	err = bitmap.Set(15)
+	if err != nil {
+		t.Error(err)
+	}
 	if !(bitmap.IsExist(15)) {
 		t.Error("UNExpected error")
 	}
 	if bitmap.IsExist(1) {
 		t.Error("UNExpected error")
 	}
+	bitmap.PrintAllBits()
 	bitmap.Destroy()
 
 }

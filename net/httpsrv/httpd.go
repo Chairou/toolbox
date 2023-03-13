1c67a95a18437e27393f6ee4b5f0d5325cfaa86f
chairou <chairou@tencent.com>
2023-03-13 23:55:40 +0800

    change go.mod module from toolbox to github.com/Chairou/toolbox

--- net/httpsrv/httpd.go
+++ net/httpsrv/httpd.go
@@ -19,6 +19,7 @@ func sayhelloName(w http.ResponseWriter, r *http.Request) {
 	}
 	fmt.Fprintf(w, "Hello Wrold!") //这个写入到w的是输出到客户端的
 }
+
 func main() {
 	http.HandleFunc("/", sayhelloName)       //设置访问的路由
 	err := http.ListenAndServe(":9090", nil) //设置监听的端口

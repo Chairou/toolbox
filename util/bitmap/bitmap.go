c9cf3508fa61629cc814ac6e56b4a481b0a90dfb
chairou <chairou@tencent.com>
2026-04-15 20:33:03 +0800

    修正bitmap的问题

--- util/bitmap/bitmap.go
+++ util/bitmap/bitmap.go
@@ -1,7 +1,6 @@
 package bitmap
 
 import (
-	"errors"
 	"fmt"
 	"sync"
 )
@@ -12,6 +11,7 @@ type BitMapStruct struct {
 	Name       string `json:"Name"`
 	ByteBuffer []byte `json:"byteBuffer"`
 	BitLength  uint64 `json:"length"`
+	mu         sync.RWMutex // 添加读写锁保证线程安全
 }
 
 const byteSize = 8 //1 byte = 8 bits
@@ -45,14 +45,17 @@ func GetBitMap(name string) *BitMapStruct {
 // Set 设置对应位置的位数为1
 // parameter m, the n bit of the bitmap
 func (bt *BitMapStruct) Set(n uint64) error {
-	if n/byteSize > uint64(len(bt.ByteBuffer)) {
-		return errors.New(fmt.Sprintf("overflow of bitmap: %d", n))
+	if n >= bt.BitLength {
+		return fmt.Errorf("bit position %d exceeds bitmap length %d", n, bt.BitLength)
 	}
+	
+	bt.mu.Lock()
+	defer bt.mu.Unlock()
+	
 	byteIndex := n / byteSize   //第x个字节（0,1,2...）
 	offsetIndex := n % byteSize //偏移量(0<偏移量<byteSize)
-	//bt[byteIndex] = bt[byteIndex] | 1<<offsetIndex //异或1（置位）
 	//第x个字节偏移量为offsetIndex的位 置位1
-	bt.ByteBuffer[byteIndex] |= 1 << offsetIndex //异或1（置位）
+	bt.ByteBuffer[byteIndex] |= 1 << offsetIndex
 	return nil
 }
 
@@ -60,11 +63,17 @@ func (bt *BitMapStruct) Set(n uint64) error {
 // Mset 一次设置多个bit
 // parameter n bit array
 func (bt *BitMapStruct) MSet(n ...uint64) error {
+	bt.mu.Lock()
+	defer bt.mu.Unlock()
+	
 	for _, v := range n {
-		err := bt.Set(v)
-		if err != nil {
-			return err
+		if v >= bt.BitLength {
+			return fmt.Errorf("bit position %d exceeds bitmap length %d", v, bt.BitLength)
 		}
+		
+		byteIndex := v / byteSize
+		offsetIndex := v % byteSize
+		bt.ByteBuffer[byteIndex] |= 1 << offsetIndex
 	}
 	return nil
 }
@@ -73,12 +82,17 @@ func (bt *BitMapStruct) MSet(n ...uint64) error {
 // Del 设置对应bit为0
 // parameter n the n bit
 func (bt *BitMapStruct) Del(n uint64) error {
-	if n/byteSize > uint64(len(bt.ByteBuffer)) {
-		return errors.New(fmt.Sprintf("overflow of bitmap: %d", n))
+	if n >= bt.BitLength {
+		return fmt.Errorf("bit position %d exceeds bitmap length %d", n, bt.BitLength)
 	}
+	
+	bt.mu.Lock()
+	defer bt.mu.Unlock()
+	
 	byteIndex := n / byteSize
 	offsetIndex := n % byteSize
-	bt.ByteBuffer[byteIndex] &= 0 << offsetIndex // zero
+	// 使用位掩码清零特定位，而不是清零整个字节
+	bt.ByteBuffer[byteIndex] &^= 1 << offsetIndex
 	return nil
 }
 
@@ -86,11 +100,17 @@ func (bt *BitMapStruct) Del(n uint64) error {
 // MDel 批量删除对应bit
 // parameter n bit array
 func (bt *BitMapStruct) MDel(elements ...uint64) error {
+	bt.mu.Lock()
+	defer bt.mu.Unlock()
+	
 	for _, v := range elements {
-		err := bt.Del(v)
-		if err != nil {
-			return err
+		if v >= bt.BitLength {
+			return fmt.Errorf("bit position %d exceeds bitmap length %d", v, bt.BitLength)
 		}
+		
+		byteIndex := v / byteSize
+		offsetIndex := v % byteSize
+		bt.ByteBuffer[byteIndex] &^= 1 << offsetIndex
 	}
 	return nil
 }
@@ -99,24 +119,32 @@ func (bt *BitMapStruct) MDel(elements ...uint64) error {
 // IsExist 返回是否置位
 // parameter n is position of bitmap
 func (bt *BitMapStruct) IsExist(n uint64) bool {
-	if n/byteSize > uint64(len(bt.ByteBuffer)) {
-		fmt.Println("overflow of bitmap: ", n)
+	if n >= bt.BitLength {
 		return false
 	}
+	
+	bt.mu.RLock()
+	defer bt.mu.RUnlock()
+	
 	byteIndex := n / byteSize
 	offsetIndex := n % byteSize
-	return bt.ByteBuffer[byteIndex]&(1<<offsetIndex) != 0 //TODO：注意：条件是 ！=0，有可能是：16,32等
+	return bt.ByteBuffer[byteIndex]&(1<<offsetIndex) != 0
 }
 
 // MExist returns true if the specified list
 // MExist 批量判断是否置位
 // parameter elements is position array of bitmap
 func (bt *BitMapStruct) MExist(elements ...uint64) map[uint64]bool {
+	bt.mu.RLock()
+	defer bt.mu.RUnlock()
+	
 	len := len(elements)
 	matchElementsList := make(map[uint64]bool, len)
 	for _, v := range elements {
-		if bt.IsExist(v) {
-			matchElementsList[v] = true
+		if v < bt.BitLength {
+			byteIndex := v / byteSize
+			offsetIndex := v % byteSize
+			matchElementsList[v] = bt.ByteBuffer[byteIndex]&(1<<offsetIndex) != 0
 		} else {
 			matchElementsList[v] = false
 		}
@@ -127,12 +155,17 @@ func (bt *BitMapStruct) MExist(elements ...uint64) map[uint64]bool {
 // PrintAllBits Print all bits and all values
 // PrintAllBits 打印整个bitmap
 func (bt *BitMapStruct) PrintAllBits() {
+	bt.mu.RLock()
+	defer bt.mu.RUnlock()
+	
 	var i uint64
-	for i = 0; i < uint64(len(bt.ByteBuffer)*8); i++ {
-		if bt.IsExist(i) {
-			fmt.Println("bit: ", i, "valL: ", 1)
+	for i = 0; i < bt.BitLength; i++ {
+		byteIndex := i / byteSize
+		offsetIndex := i % byteSize
+		if bt.ByteBuffer[byteIndex]&(1<<offsetIndex) != 0 {
+			fmt.Println("bit: ", i, "val: ", 1)
 		} else {
-			fmt.Println("bit: ", i, "valL: ", 0)
+			fmt.Println("bit: ", i, "val: ", 0)
 		}
 	}
 }
@@ -140,13 +173,21 @@ func (bt *BitMapStruct) PrintAllBits() {
 // Clean set all bit zero
 // Clean 清零所有位
 func (bt *BitMapStruct) Clean() {
-	bt.ByteBuffer = make([]byte, bt.BitLength/byteSize+1)
-	//bt.ByteBuffer = bt.ByteBuffer[:0]
+	bt.mu.Lock()
+	defer bt.mu.Unlock()
+	
+	// 清零现有内存而不是重新分配
+	for i := range bt.ByteBuffer {
+		bt.ByteBuffer[i] = 0
+	}
 }
 
 // Destroy destory current bitmap instance
 // Destroy 删除当前bitmap
 func (bt *BitMapStruct) Destroy() {
+	bt.mu.Lock()
+	defer bt.mu.Unlock()
+	
 	bt.BitLength = 0
 	bt.ByteBuffer = nil
 	bitMapStore.Delete(bt.Name)
